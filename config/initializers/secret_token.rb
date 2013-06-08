fb9bf50230ef1dbb9d1ce8cb7fd4ff4c08e9ed61
Atef Haque <atefth@gmail.com>
2013-06-08 13:05:23 +0600

    Initail Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Barter::Application.config.secret_token = '99b4aae2a1e636f2bf3fa4d7315d8e73a6b91bc744d62dbfdaccfc460dabf37f7aba233e9b58962b775e9a01bc3e9ec83963d121cd68859f16f0d58ec72dd33a'
